bc8b67fed5ce69655b83173ba2b0757be5192f16
Josh Newans <josh.newans@gmail.com>
2026-01-01 11:50:37 +1100

    Move toxml() to earlier line

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -18,10 +18,10 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = xacro.process_file(xacro_file).toxml()
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
